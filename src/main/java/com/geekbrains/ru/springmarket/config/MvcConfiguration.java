780aab79dee80eee1d28b29a75c8b1753632e69b
Tatiana Nosova <tatiananosova746@gmail.com>
2021-10-04 10:34:20 -0500

    Homework4

--- src/main/java/com/geekbrains/ru/springmarket/config/MvcConfiguration.java
+++ src/main/java/com/geekbrains/ru/springmarket/config/MvcConfiguration.java
@@ -1,9 +1,14 @@
 package com.geekbrains.ru.springmarket.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
 
 import java.nio.file.Paths;
 
@@ -18,6 +23,35 @@ public class MvcConfiguration implements WebMvcConfigurer {
 
         registry.addResourceHandler("/" + dirName + "/**")
                 .addResourceLocations("file:/"+  staticPath + "/");
+    }
+    @Bean(name = "categories")
+    public DefaultWsdl11Definition categoriesWsdl11Definition(XsdSchema categoriesSchema) {
+        DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
+        wsdl11Definition.setPortTypeName("CategoriesPort");
+        wsdl11Definition.setLocationUri("/ws");
+        wsdl11Definition.setTargetNamespace("http://www.geekbrains.com/springmarket/ws/categories");
+        wsdl11Definition.setSchema(categoriesSchema);
+        return wsdl11Definition;
+    }
+
+    @Bean(name = "products")
+    public DefaultWsdl11Definition productsWsdl11Definition(XsdSchema productsSchema) {
+        DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
+        wsdl11Definition.setPortTypeName("ProductsPort");
+        wsdl11Definition.setLocationUri("/ws");
+        wsdl11Definition.setTargetNamespace("http://www.geekbrains.com/springmarket/ws/products");
+        wsdl11Definition.setSchema(productsSchema);
+        return wsdl11Definition;
+    }
+
+    @Bean
+    public XsdSchema categoriesSchema() {
+        return new SimpleXsdSchema(new ClassPathResource("categories.xsd"));
+    }
+
+    @Bean
+    public XsdSchema productsSchema() {
+        return new SimpleXsdSchema(new ClassPathResource("products.xsd"));
     }
 
 }
